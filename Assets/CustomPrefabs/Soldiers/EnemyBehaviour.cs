255970b01b7d7705395e583f8740a9474d2572b6
giosuemazzola <mazzolagiosue95@gmail.com>
2023-12-14 13:17:27 +0100

    enemyAnimation1

---
+++ Assets/CustomPrefabs/Soldiers/EnemyBehaviour.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehaviour : MonoBehaviour
+{
+    [SerializeField] GameObject enemySoldier;
+    Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = enemySoldier.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        animator.SetFloat("Speed", 0.2f);
+      
+    }
+}
