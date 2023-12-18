db6da8c63e65f8f34cc79a5c40d65442c860b044
giosuemazzola <mazzolagiosue95@gmail.com>
2023-12-18 13:11:38 +0100

    createPath1

--- Assets/Scripts/Enemy/Move.cs
+++ Assets/Scripts/Enemy/Move.cs
@@ -1,14 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class Move : MonoBehaviour
 {
     [SerializeField] NavMeshAgent agent;
-    [SerializeField] public Transform target;
+    public Transform destinationTarget;
+    public Transform goBackTarget;
 
-    
+
     private void Awake()
     {
         agent = GetComponent<NavMeshAgent>();
@@ -16,12 +18,30 @@ public class Move : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        agent.destination = target.position;
+        agent.destination = destinationTarget.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        OnTriggerEnter(destinationTarget.GetComponent<Collider>());
+        yield return OnTriggerEnter()
+    }
+
+    public void GoToTarget(Transform target)
+    {
+        agent.destination = target.position;
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.tag.Equals("checkPoint"))
+        {
+            GoToTarget(other.transform);
+        }
+    }
+    IEnumerator GoBack()
+    {
+        yield return OnTriggerEnter();
     }
 }
