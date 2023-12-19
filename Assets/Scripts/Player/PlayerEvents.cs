448de47fee8ecc73fac7192c2e67147ae98e6382
Eduardo Meli <MLEDRD02E15C351O@studium.unict.it>
2023-12-19 18:33:24 +0100

    added comment on PlayerEvents

--- Assets/Scripts/Player/PlayerEvents.cs
+++ Assets/Scripts/Player/PlayerEvents.cs
@@ -7,6 +7,7 @@ public class PlayerEvent : MonoBehaviour
     public GameObject _object;
     private void Update()
     {
+        // This is a debug event. Remove in final release!
         if(Input.GetKeyDown(KeyCode.E))
         {
             Vector3 pos = transform.position;
