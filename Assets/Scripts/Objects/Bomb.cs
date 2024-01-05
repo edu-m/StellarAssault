e9e7a114ce4cb60ca3ce35acd3f10de6deb7d9c8
Eduardo Meli <MLEDRD02E15C351O@studium.unict.it>
2024-01-05 16:35:58 +0100

    added sound to bomb placement

--- Assets/Scripts/Objects/Bomb.cs
+++ Assets/Scripts/Objects/Bomb.cs
@@ -8,23 +8,31 @@ public class Bomb : MonoBehaviour, IInteractable
 {
     [SerializeField] GameObject bomb;
     [SerializeField] GameObject bombOverlay;
+    [SerializeField] AudioSource bombAudioSource;
 
     public string GetDescription()
     {
         return "Place Bomb";
     }
 
-    public void Interact()
-    {
+    IEnumerator PlayAndDeactivate() {
+        bombAudioSource.Play();
+        yield return new WaitForSeconds(bombAudioSource.clip.length);
         PlayerData.SetObject(false);
         bomb.SetActive(true);
         bombOverlay.SetActive(false);
         gameObject.SetActive(false);
     }
 
+    public void Interact()
+    {
+        StartCoroutine(PlayAndDeactivate());
+    }
+
     // Update is called once per frame
     public void Start()
     {
+        //bombAudioSource = GetComponent<AudioSource>();  
         PlayerData.SetObject(true);
         bombOverlay.SetActive(true);
     }
